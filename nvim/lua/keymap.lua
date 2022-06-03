2b5975fa4c845807a8702c47ff74849859aab452
Lay <lei4519@126.com>
2022-06-03 16:09:39 +0800

    feat: clear lsp files

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -216,28 +216,6 @@ local keys = {
         D = { "<Plug>VimspectorRunToCursor", "运行到光标" },
         i = { "<Plug>VimspectorStepInto", "踏入" },
         o = { "<Plug>VimspectorStepOut", "跳出当前功能范围" },
-
-        -- R = { "<cmd>lua require'dap'.run_to_cursor()<cr>", "Run to Cursor" },
-        -- E = { "<cmd>lua require'dapui'.eval(vim.fn.input '[Expression] > ')<cr>", "Evaluate Input" },
-        -- C = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input '[Condition] > ')<cr>", "Conditional Breakpoint" },
-        -- U = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle UI" },
-        -- b = { "<cmd>lua require'dap'.step_back()<cr>", "Step Back" },
-        -- c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-        -- d = { "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect" },
-        -- e = { "<cmd>lua require'dapui'.eval()<cr>", "Evaluate" },
-        -- g = { "<cmd>lua require'dap'.session()<cr>", "Get Session" },
-        -- h = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Hover Variables" },
-        -- S = { "<cmd>lua require'dap.ui.widgets'.scopes()<cr>", "Scopes" },
-        -- i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-        -- o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-        -- p = { "<cmd>lua require'dap'.pause.toggle()<cr>", "Pause" },
-        -- -- p = { "<cmd>lua require'dap'.pause()<cr>", "Pause" },
-        -- q = { "<cmd>lua require'dap'.close()<cr>", "Quit" },
-        -- r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "Toggle Repl" },
-        -- s = { "<cmd>lua require'dap'.continue()<cr>", "Start" },
-        -- t = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-        -- x = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
-        -- u = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" },
       },
 
       e = { "<Cmd>CocCommand explorer<CR>", "Explorer" },
@@ -280,7 +258,7 @@ local keys = {
       n = { "<CMD>ene!<CR>", "New File" },
 
       -- SymbolOutLine
-      o = { ":SymbolsOutline<cr>", "Symbols Outline" },
+      o = { ":CocOutline<cr>", "Symbols Outline" },
 
       p = {
         name = "Packer",
