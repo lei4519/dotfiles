9e69584ab8a730155107573b7354ff90e9c1ad1a
Lay <lei4519@126.com>
2023-09-18 19:59:37 +0800

    feat: todo-comment

---
+++ lazy-vim/lua/plugins/todo-comments.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/todo-comments.nvim",
+  opts = {
+    highlight = {
+      pattern = [[.*<(KEYWORDS)\s*:?]], -- pattern or table of patterns, used for highlighting (vim regex)
+    },
+    search = {
+      pattern = [[\b(KEYWORDS)\s*:?]], -- ripgrep regex
+    },
+  },
+}
