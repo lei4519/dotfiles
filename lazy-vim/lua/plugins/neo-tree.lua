2089d8c54450a130b23d274ae0907a4eb2a5bd77
lijinlei <lei4519@126.com>
2023-09-22 18:20:55 +0800

    config

--- lazy-vim/lua/plugins/neo-tree.lua
+++ lazy-vim/lua/plugins/neo-tree.lua
@@ -6,6 +6,12 @@ return {
         hide_dotfiles = false,
         hide_gitignored = false,
       },
+      window = {
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          ["<C-j>"] = "move_cursor_down",
+          ["<C-k>"] = "move_cursor_up",
+        },
+      },
     },
   },
 }
