4162955e24cd2873697f1355c9bf5130eabfbc0e
Lay <lei4519@126.com>
2023-09-22 13:11:01 +0800

    feat: tmux keymap

--- lazy-vim/lua/config/lazy.lua
+++ lazy-vim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.rust" },
 
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
