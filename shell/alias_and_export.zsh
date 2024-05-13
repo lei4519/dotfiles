d3de66d5f7f543b7f5ad860f49d45dc90cd19a2b
lijinlei <lijinlei@zhihu.com>
2024-05-13 18:08:33 +0800

    feat: alias

--- shell/alias_and_export.zsh
+++ shell/alias_and_export.zsh
@@ -28,6 +28,11 @@ export YAZI_CONFIG_HOME="$HOME/dotfiles/terminal/file-manager/yazi"
 alias  c='clear' # clear terminal
 alias vc='code' # gui code editor
 
+alias  l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
 # Handy change dir shortcuts
 alias ..='cd ..'
 alias ...='cd ../..'
--- shell/linux-os.zsh
+++ shell/linux-os.zsh
@@ -47,11 +47,6 @@ function in {
     fi
 }
 
-alias  l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
 alias un='$aurhelper -Rns' # uninstall package
 alias up='$aurhelper -Syu' # update system/package/aur
 alias pl='$aurhelper -Qs' # list installed package
