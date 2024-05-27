46ac453fa4b0ca7925d85ad959a7d8d343e56a56
lijinlei <lijinlei@zhihu.com>
2024-05-27 19:03:03 +0800

    feat: hardtime

---
+++ nvim/lazy-vim/lua/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+-- https://github.com/tris203/precognition.nvim
+-- 改正 vim 的使用习惯
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {},
+}
