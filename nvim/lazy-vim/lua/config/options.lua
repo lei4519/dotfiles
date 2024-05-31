89bd709dc5cf051da93542023f0314ad54cf3e3a
lijinlei <lijinlei@zhihu.com>
2024-05-31 14:07:07 +0800

    update

--- nvim/lazy-vim/lua/config/options.lua
+++ nvim/lazy-vim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.foldmethod = "indent"
--- vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
-vim.opt.foldlevel = 99
+-- vim.opt.foldmethod = "indent"
+-- -- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- -- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+-- vim.opt.foldlevel = 99
