f5f62f116677fa32fb9ff4070410fab24a692df0
lijinlei <lijinlei@zhihu.com>
2024-08-11 22:45:08 +0800

    neotree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -1,65 +1,7 @@
-local function getTelescopeOpts(state, path)
-  return {
-    cwd = path,
-    search_dirs = { path },
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require("telescope.actions")
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local action_state = require("telescope.actions.state")
-        local selection = action_state.get_selected_entry()
-        local filename = selection.filename
-        if filename == nil then
-          filename = selection[1]
-        end
-        -- any way to open the file without triggering auto-close event of neo-tree?
-        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-      end)
-      return true
-    end,
-  }
-end
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    window = {
-      mappings = {
-        -- ["/"] = "none",
-        -- ["t"] = "none",
-        -- ["R"] = "search_by_grugfar",
-        ["f"] = "filter_on_submit",
-        ["tf"] = "fzf_find",
-        ["tg"] = "fzf_grep",
-        ["h"] = function(state)
-          local node = state.tree:get_node()
-          if node.type == "directory" and node:is_expanded() then
-            require("neo-tree.sources.filesystem").toggle_directory(state, node)
-          else
-            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-          end
-        end,
-        ["l"] = function(state)
-          local node = state.tree:get_node()
-          if node.type == "directory" then
-            if not node:is_expanded() then
-              require("neo-tree.sources.filesystem").toggle_directory(state, node)
-            elseif node:has_children() then
-              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
-            end
-          end
-        end,
-      },
-    },
     commands = {
-      search_by_spectre = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        require("spectre").open({
-          is_insert_mode = true,
-          cwd = path,
-        })
-      end,
       search_by_grugfar = function(state)
         local node = state.tree:get_node()
         local path = node:get_id()
@@ -68,16 +10,6 @@ return {
             filesFilter = path,
           },
         })
-      end,
-      telescope_find = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        require("telescope.builtin").find_files(getTelescopeOpts(state, path))
-      end,
-      telescope_grep = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
       end,
       fzf_find = function(state)
         local node = state.tree:get_node()
@@ -91,29 +23,159 @@ return {
         -- require("fzf-lua").live_grep({ cwd = path })
       end,
     },
-    filesystem = {
-      never_show = {
-        ".DS_Store",
-        "thumbs.db",
+    window = {
+      mappings = {
+        ["<space>"] = {
+          "toggle_node",
+          nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+        },
+        ["<2-LeftMouse>"] = "open",
+        ["<cr>"] = "open",
+        ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+        ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+        -- Read `# Preview Mode` for more information
+        ["l"] = "focus_preview",
+        ["S"] = "open_split",
+        ["s"] = "open_vsplit",
+        -- ["S"] = "split_with_window_picker",
+        -- ["s"] = "vsplit_with_window_picker",
+        ["t"] = "open_tabnew",
+        -- ["<cr>"] = "open_drop",
+        -- ["t"] = "open_tab_drop",
+        ["w"] = "open_with_window_picker",
+        --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+        ["C"] = "close_node",
+        -- ['C'] = 'close_all_subnodes',
+        ["z"] = "close_all_nodes",
+        --["Z"] = "expand_all_nodes",
+        ["a"] = {
+          "add",
+          -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+          -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+          config = {
+            show_path = "none", -- "none", "relative", "absolute"
+          },
+        },
+        ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+        ["d"] = "delete",
+        ["r"] = "rename",
+        ["y"] = "copy_to_clipboard",
+        ["x"] = "cut_to_clipboard",
+        ["p"] = "paste_from_clipboard",
+        ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+        -- ["c"] = {
+        --  "copy",
+        --  config = {
+        --    show_path = "none" -- "none", "relative", "absolute"
+        --  }
+        --}
+        ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+        ["q"] = "close_window",
+        ["R"] = "refresh",
+        ["?"] = "show_help",
+        ["<"] = "prev_source",
+        [">"] = "next_source",
+        ["i"] = "show_file_details",
       },
+    },
+    filesystem = {
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          ".DS_Store",
+          "thumbs.db",
+        },
       },
-      -- follow_current_file = {
-      --   enabled = true,
-      -- },
-      group_empty_dirs = true,
-      -- window = {
-      --   fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-      --     ["<C-j>"] = "move_cursor_down",
-      --     ["<C-k>"] = "move_cursor_up",
-      --   },
-      -- },
+      window = {
+        mappings = {
+          ["ts"] = "search_by_grugfar",
+          ["tf"] = "fzf_find",
+          ["tg"] = "fzf_grep",
+          ["h"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" and node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
+          end,
+          ["l"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+              if not node:is_expanded() then
+                require("neo-tree.sources.filesystem").toggle_directory(state, node)
+              elseif node:has_children() then
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            end
+          end,
+          ["<bs>"] = "navigate_up",
+          ["."] = "set_root",
+          ["H"] = "toggle_hidden",
+          ["/"] = "fuzzy_finder",
+          ["D"] = "fuzzy_finder_directory",
+          ["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
+          -- ["D"] = "fuzzy_sorter_directory",
+          ["f"] = "filter_on_submit",
+          ["<c-x>"] = "clear_filter",
+          ["[g"] = "prev_git_modified",
+          ["]g"] = "next_git_modified",
+          ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+          ["oc"] = { "order_by_created", nowait = false },
+          ["od"] = { "order_by_diagnostics", nowait = false },
+          ["og"] = { "order_by_git_status", nowait = false },
+          ["om"] = { "order_by_modified", nowait = false },
+          ["on"] = { "order_by_name", nowait = false },
+          ["os"] = { "order_by_size", nowait = false },
+          ["ot"] = { "order_by_type", nowait = false },
+          -- ['<key>'] = function(state) ... end,
+        },
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          ["<down>"] = "move_cursor_down",
+          ["<C-n>"] = "move_cursor_down",
+          ["<up>"] = "move_cursor_up",
+          ["<C-p>"] = "move_cursor_up",
+          -- ['<key>'] = function(state, scroll_padding) ... end,
+        },
+      },
     },
-  },
-  keys = {
-    { "<leader>e", "<cmd>Oil --float<CR>", desc = "Explorer (Root Dir)", remap = true },
-    -- { "<leader>E", "<leader>fM", desc = "Explorer (cwd)", remap = true },
+    buffers = {
+      window = {
+        mappings = {
+          ["bd"] = "buffer_delete",
+          ["<bs>"] = "navigate_up",
+          ["."] = "set_root",
+          ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+          ["oc"] = { "order_by_created", nowait = false },
+          ["od"] = { "order_by_diagnostics", nowait = false },
+          ["om"] = { "order_by_modified", nowait = false },
+          ["on"] = { "order_by_name", nowait = false },
+          ["os"] = { "order_by_size", nowait = false },
+          ["ot"] = { "order_by_type", nowait = false },
+        },
+      },
+    },
+    git_status = {
+      window = {
+        -- position = "float",
+        mappings = {
+          ["A"] = "git_add_all",
+          ["gu"] = "git_unstage_file",
+          ["ga"] = "git_add_file",
+          ["gr"] = "git_revert_file",
+          ["gc"] = "git_commit",
+          ["gp"] = "git_push",
+          ["gg"] = "git_commit_and_push",
+          ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+          ["oc"] = { "order_by_created", nowait = false },
+          ["od"] = { "order_by_diagnostics", nowait = false },
+          ["om"] = { "order_by_modified", nowait = false },
+          ["on"] = { "order_by_name", nowait = false },
+          ["os"] = { "order_by_size", nowait = false },
+          ["ot"] = { "order_by_type", nowait = false },
+        },
+      },
+    },
   },
 }
